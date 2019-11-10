8e22c8fa56d29211fa152936845fba5ed110c7b0
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-11-10 00:01:21 -0500

    Update Leetcode_716_solution_1.py
    Added a second implementation using stack + heap + boolean flag

--- Max Stack/Leetcode_716_solution_1.py
+++ Max Stack/Leetcode_716_solution_1.py
@@ -1,39 +1,91 @@
-# top 36%  - while implemention is correct, there is plenty of room to improve efficiency!
+# solution using stack + heap + boolean flag DS
+# top 0.5%
+from heapq import heappush, heappop
+
+class Node:
+    def __init__(self, v: int):
+        self.v = v
+        self.removed = False
+    def __repr__(self):
+        return "{v} ({removed})".format(v=self.v, removed=self.removed)
+    
 class MaxStack:
-
+    
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.stack  = []
+        self.heap = []
+        self.i = 0
         
-
     def push(self, x: int) -> None:
-        if self.stack: self.stack.append((x, max(x, self.peekMax())))
-        else: self.stack.append((x, x))
-
+        self.i += 1
+        node = Node(x)
+        self.stack.append(node)
+        heappush(self.heap, (-x, -self.i, node))
+        
     def pop(self) -> int:
-        return self.stack.pop()[0]
-
+        while True:
+            node = self.stack.pop()
+            if node.removed:
+                continue
+            node.removed = True
+            return node.v
+        
     def top(self) -> int:
-        return self.stack[-1][0]
-
+        while True:
+            if self.stack[-1].removed:
+                self.stack.pop()
+                continue
+            return self.stack[-1].v
+        
     def peekMax(self) -> int:
-        return self.stack[-1][1] if self.stack else None
-
+        while True:
+            if self.heap[0][2].removed:
+                heappop(self.heap)
+                continue
+            return self.heap[0][2].v
+        
     def popMax(self) -> int:
-        if not self.stack: return None
-        stack2 = []
-        curMax = self.peekMax()
-        while self.stack[-1][1] == curMax:
-            x = self.stack.pop()
-            if x[0] < curMax: stack2.append(x)
-            else: break
-        while stack2:
-            self.push(stack2.pop()[0])
-        return curMax
+        while True:
+            _, _, node = heappop(self.heap)
+            if node.removed:
+                continue
+            node.removed = True
+            return node.v
+        
+# 1st attempt: top 36%
+# class MaxStack:
+
+#     def __init__(self):
+#         """
+#         initialize your data structure here.
+#         """
+#         self.stack  = []
         
 
+#     def push(self, x: int) -> None:
+#         if self.stack: self.stack.append((x, max(x, self.peekMax())))
+#         else: self.stack.append((x, x))
+
+#     def pop(self) -> int:
+#         return self.stack.pop()[0]
+
+#     def top(self) -> int:
+#         return self.stack[-1][0]
+
+#     def peekMax(self) -> int:
+#         return self.stack[-1][1] if self.stack else None
+
+#     def popMax(self) -> int:
+#         if not self.stack: return None
+#         stack2 = []
+#         curMax = self.peekMax()
+#         while self.stack[-1][1] == curMax:
+#             x = self.stack.pop()
+#             if x[0] < curMax: stack2.append(x)
+#             else: break
+#         while stack2:
+#             self.push(stack2.pop()[0])
+#         return curMax
 
 # Your MaxStack object will be instantiated and called as such:
 # obj = MaxStack()
@@ -42,3 +94,4 @@ class MaxStack:
 # param_3 = obj.top()
 # param_4 = obj.peekMax()
 # param_5 = obj.popMax()
+# top 36%  - while implemention is correct, there is plenty of room to improve efficiency!
