24d9661a37487a5d932d53979758f4e36e0b1680
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-11-09 23:15:49 -0500

    Create Leetcode_716.py
    Solution using a stack augumented with current maximum info for each element

---
+++ Max Stack/Leetcode_716.py
@@ -0,0 +1,43 @@
+class MaxStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack  = []
+        
+
+    def push(self, x: int) -> None:
+        if self.stack: self.stack.append((x, max(x, self.peekMax())))
+        else: self.stack.append((x, x))
+
+    def pop(self) -> int:
+        return self.stack.pop()[0]
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def peekMax(self) -> int:
+        return self.stack[-1][1] if self.stack else None
+
+    def popMax(self) -> int:
+        if not self.stack: return None
+        stack2 = []
+        curMax = self.peekMax()
+        while self.stack[-1][1] == curMax:
+            x = self.stack.pop()
+            if x[0] < curMax: stack2.append(x)
+            else: break
+        while stack2:
+            self.push(stack2.pop()[0])
+        return curMax
+        
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
