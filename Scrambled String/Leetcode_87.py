ed86094f8440e59efce172e07b09a3fe8fca16ef
arw2019 <andrew.r.wieteska@gmail.com>
2019-11-17 13:35:12 -0500

    a much faster DP implementation

--- Scrambled String/Leetcode_87.py
+++ Scrambled String/Leetcode_87.py
@@ -1,3 +1,22 @@
+# top 2%
+# DP with memoization + pruning same as below
+# passing memo table around as function argument gives massive (10X) speedup
+from collections import Counter
+class Solution:
+    def isScramble(self, s1: str, s2: str, memo: dict = {}) -> bool:
+        if len(s1) != len(s2) or sorted(s1) != sorted(s2):
+            return False
+        if len(s1) == 1:
+            return s1 == s2
+        if (s1,s2) in memo:
+            return memo[s1, s2]
+        for i in range(1, len(s1)):
+            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:],s2[i:])) or \
+                (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:],s2[:-i])):
+                memo[s1, s2] = True
+                return True
+        memo[s1, s2]=False
+        return False
 # top 30%
 # DP with memoization + subproblem pruning  
 from collections import Counter
