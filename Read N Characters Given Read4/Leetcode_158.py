058deb5c1b0772fa80ad3d0e6488143a8f83bdc8
arw2019 <andrew.r.wieteska@gmail.com>
2019-11-17 16:00:38 -0500

    different, slighly faster implementation

--- Read N Characters Given Read4/Leetcode_158.py
+++ Read N Characters Given Read4/Leetcode_158.py
@@ -1,3 +1,38 @@
+# top 0.6%
+# same idea but implementation more efficient
+class Solution:
+    
+    def __init__(self):
+        self._cache = []
+        
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        cnt = math.ceil((n-len(self._cache))/4)
+        
+        while cnt > 0:
+            
+            buf4 = [''] * 4
+            read4(buf4)
+            
+            self._cache += buf4
+            
+            cnt -= 1
+            
+        tot_chars = n
+        for i in range(n):
+            if self._cache[i] == '':
+                tot_chars = i
+                break
+            buf[i] = self._cache[i]
+        
+        self._cache = self._cache[n:]
+        return tot_chars
+        
+
 # top 10%
 class Solution:
     
