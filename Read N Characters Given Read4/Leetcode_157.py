250fee0ef96be9d93c93cf42e26201ad78c03010
arw2019 <andrew.r.wieteska@gmail.com>
2019-11-17 14:01:36 -0500

    iterate through buffer until EOF

---
+++ Read N Characters Given Read4/Leetcode_157.py
@@ -0,0 +1,33 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf, a list of characters
+    @return an integer
+    def read4(buf):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+# top 0.75%
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        tot_chars = 0
+        while tot_chars < n:
+            buf4 = ['']*4
+            num_chars = read4(buf4)    
+            if num_chars == 0: 
+                break # EOF
+            num_chars = min(num_chars, n-tot_chars)
+            buf[tot_chars:]= buf4
+            tot_chars += num_chars
+        return tot_chars
