a23a4d562ee5178c45156155bb61ed436a1be576
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-11-19 17:28:33 -0500

    Update Leetcode_51.py
    rename depth-first-search function to dfs

--- Solve N Queens/Leetcode_51.py
+++ Solve N Queens/Leetcode_51.py
@@ -4,7 +4,7 @@ class Solution:
         self.result: List[List[int]] = []
         self.col_placement = [0] * n
         
-        def solve_n_queens(row: int) -> None:
+        def dfs(row: int) -> None:
             if row == n:
                 # all the queens are legally placed
                 self.result += [self.col_placement.copy()]
@@ -15,8 +15,8 @@ class Solution:
                         for i, c in enumerate(self.col_placement[:row])
                 ):
                     self.col_placement[row] = col
-                    solve_n_queens(row+1)
+                    dfs(row+1)
         
-        solve_n_queens(0)
+       dfs(0)
         
         return [ ['.' * i + 'Q' + '.'*(n-i-1) for i in solution] for solution in self.result]
