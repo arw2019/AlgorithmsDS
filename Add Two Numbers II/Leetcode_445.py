813ec116cf2bd6c7593df4edca53853c13a20fba
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-12-07 21:37:20 -0500

    Create Leetcode_445.py
    reverse both lists, then add reversed lists and reverse the result

---
+++ Add Two Numbers II/Leetcode_445.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# top 0.7%
+class Solution:
+    
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur, prev = head, None
+        while cur:
+            nextNode = cur.next
+            cur.next = prev
+            prev, cur = cur, nextNode
+        return prev
+    
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        l1, l2 = self.reverseList(l1), self.reverseList(l2)
+        
+        dummy = ListNode(-1)
+        res = dummy
+        carryover = 0
+        while l1 or l2:
+            cur = carryover
+            if l1: 
+                cur += l1.val
+                l1 = l1.next
+            if l2:
+                cur += l2.val
+                l2 = l2.next
+            cur, carryover = cur % 10, cur // 10
+            res.next = ListNode(cur)
+            res = res.next
+        if carryover: res.next = ListNode(carryover)
+        
+        return self.reverseList(dummy.next)
