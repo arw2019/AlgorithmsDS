9fe6cc666d0215ae334b67235479e7f9a71347e0
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-12-11 10:48:54 +0000

    NlgN solution using binary search

--- Maximum Average Subarray/Leetcode_644.py
+++ Maximum Average Subarray/Leetcode_644.py
@@ -1,3 +1,27 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        
+        def isBigger(x: int) -> bool:
+            """finds whether nums contains a subarray with average >= x in O(N) time"""
+            cur = sum([nums[i] - x for i in range(k)])
+            if cur > 0: return True
+            prev, prevSmallest = 0, 0
+            for i in range(k, len(nums)):
+                cur += (nums[i] - x)
+                prev += (nums[i-k] - x)
+                prevSmallest = min(prev, prevSmallest)
+                if cur >= prevSmallest: return True
+            return False
+        
+        # binary search on the result using isBigger for the compare function
+        # isBinary takes  O(N) time => total time complexity is O(NlgN)
+        lo, hi = min(nums), max(nums)    
+        while hi - lo >= 1e-5:
+            mid = (lo+hi)/2.
+            if isBigger(mid): lo = mid
+            else: hi = mid      
+        return lo
+
 # brute force solution
 # O(N^2) time
 # TLE
