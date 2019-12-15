394a6cfba8289752b29802f18d186555a387583c
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-12-15 20:54:16 +0000

    O(N) time solution with O(N) extra space

--- Max Chunks To Make Sorted/Leetcode_768.py
+++ Max Chunks To Make Sorted/Leetcode_768.py
@@ -1,5 +1,18 @@
+# O(N) time solution
+# but O(N) extra space
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        N = len(arr)
+        left_max, right_min = [arr[0]]*N, [arr[-1]]*N
+        for i in range(1, N):
+            left_max[i] = max(arr[i], left_max[i-1])
+        for i in range(N-2, -1, -1):
+            right_min[i] = min(arr[i], right_min[i+1])
+        return 1 + sum(left_max[i] <= right_min[i+1] for i in range(N-1))
+  
 # AC but slow
 # O(NlgN) time complexity for worst case input
+# O(N) extra space in worst case but much better than that amortized
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         arr2 = sorted(arr)
