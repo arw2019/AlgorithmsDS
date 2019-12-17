f4875e9371bd858b8a1b61f344ec325d69cfd082
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-12-17 09:15:53 +0000

    Update Leetcode_616.py

--- Add Bold Tag in String/Leetcode_616.py
+++ Add Bold Tag in String/Leetcode_616.py
@@ -1,3 +1,32 @@
+# improve speed by storing only boundaries of bold tags
+from collections import defaultdict
+class Solution:
+    def addBoldTag(self, s: str, dictionary: List[str]) -> str:
+        if not dictionary or not s: return s
+        
+        d = defaultdict(list)
+        for word in dictionary:
+            d[word[0]].append(word)
+        mask = []
+        for i, char in enumerate(s):
+            for word in d[char]:
+                wl = len(word)
+                if s[i:i+wl] == word:
+                    if mask and mask[-1] >= i-1:
+                        mask[-1] = max(mask[-1], i+wl-1)
+                    else:
+                        mask.extend([i,i+wl-1])
+        if not mask: return s
+        res = [s[:mask[0]]]
+        for i in range(0, len(mask), 2):
+            res.extend(['<b>',s[mask[i]:mask[i+1]+1],'</b>'])
+            if i+2 < len(mask):
+                res.append(s[mask[i+1]+1:mask[i+2]])
+            else:
+                res.append(s[mask[i+1]+1:])
+        return ''.join(res)
+    
+#### correct but relatively slow solution
 import re
 class Solution:
     def addBoldTag(self, s: str, dictionary: List[str]) -> str:
