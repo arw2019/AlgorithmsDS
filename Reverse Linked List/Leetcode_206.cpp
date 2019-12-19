cb30583affcd2fc72e4c3f47d23bdae6514965b1
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-12-19 22:02:25 +0000

    recursive solution

---
+++ Reverse Linked List/Leetcode_206.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+ // recursive
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if (!head || !(head->next))
+        {
+            return head;
+        }
+        
+        ListNode* tmp = reverseList(head->next);
+        head -> next -> next = head;
+        head -> next = NULL;
+        return tmp;
+    }
+};
