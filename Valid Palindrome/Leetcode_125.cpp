ce2f97b14d5da90f4bfff05014872300a3ae5351
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-12-19 23:16:54 +0000

    two pointers solution

---
+++ Valid Palindrome/Leetcode_125.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.size() < 2) return true;
+        for (int i = 0, j = s.size()-1; i < j; i++, j--){
+            while(!isalnum(s[i]) && i<j) i++;
+            while(!isalnum(s[j]) && i<j) j--;
+            if (toupper(s[i]) != toupper(s[j])) return false; 
+        }
+        return true;
+    }
+};
