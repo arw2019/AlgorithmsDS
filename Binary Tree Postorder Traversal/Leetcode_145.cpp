2d39742a429f5e0bed56cf93d9949b6b24d10813
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2019-12-24 10:25:45 +0000

    recursive solution

---
+++ Binary Tree Postorder Traversal/Leetcode_145.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (root == NULL) {
+            return vector<int> {};
+        }
+        if (root->left == NULL and root ->right == NULL){
+            return vector<int> {root->val};
+        }
+        vector<int> ans;
+        vector<int> ansLeft = postorderTraversal(root->left);
+        ans.insert(ans.end(), ansLeft.begin(), ansLeft.end());
+        vector<int> ansRight = postorderTraversal(root->right);
+        ans.insert(ans.end(), ansRight.begin(), ansRight.end());
+        ans.push_back(root->val);
+        
+        return ans;
+        
+    }
+};
