5e96e978f998dfc452e0c95908d3a09c62552cdc
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-11 19:36:56 -0500

    added a single pass solution

--- Count Square Submatrices with All Ones/Leetcode_1277.py
+++ Count Square Submatrices with All Ones/Leetcode_1277.py
@@ -1,3 +1,22 @@
+# much faster (~600ms) single pass solution
+
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        
+        m, n = len(matrix), len(matrix[0])
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] and matrix[i-1][j] \
+                        and matrix[i][j-1] and matrix[i-1][j-1]:
+                    matrix[i][j] = min(matrix[i-1][j], 
+                                   matrix[i][j-1], 
+                                   matrix[i-1][j-1]) + 1
+        
+        return sum(matrix[i][j] for i in range(m) for j in range(n))
+
+# correct but relatively slow (~1200ms)
+
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         
