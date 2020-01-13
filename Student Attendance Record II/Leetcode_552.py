4bb428ff042cd1b5f776052ef7d8a38025826635
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-12 19:41:27 -0500

    added a bottom-up solution using a state machine

--- Student Attendance Record II/Leetcode_552.py
+++ Student Attendance Record II/Leetcode_552.py
@@ -1,3 +1,32 @@
+# bottom-up state machine solution
+
+from collections import namedtuple
+
+class Solution:
+    def checkRecord(self, n: int) -> int:
+        
+        debug = False
+        
+        mod = lambda x: x % (10**9 + 7)
+        
+        State = namedtuple('State', 'A0L0 A0L1 A0L2 A1L0 A1L1 A1L2')
+        
+        cur = State(1, 1, 0, 1, 0, 0)
+        
+        for _ in range(2, n+1):
+            cur = State(
+                mod(sum(cur[:3])),
+                cur.A0L0,
+                cur.A0L1,
+                mod(sum(cur)),
+                cur.A1L0,
+                cur.A1L1
+            )
+            if debug: print(cur)
+            
+        return mod(sum(cur))
+
+
 # top-down
 # correct but excessive recursion depth for n > 40000
 
