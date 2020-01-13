ab40eb96c6751e0588d6e0a4c4020d8eb032942f
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-12 19:54:46 -0500

    solution with guaranteed constant time query

--- Range Query - Immutable/Leetcode_303.py
+++ Range Query - Immutable/Leetcode_303.py
@@ -1,3 +1,20 @@
+# precompute sums in O(N) time
+# query is constant time
+
+from itertools import accumulate
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sums = [0] + list(accumulate(nums))
+        
+    def sumRange(self, i: int, j: int) -> int:
+        return self.sums[j+1] - self.sums[i]
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
+
 # completely brute force with caching
 # AC
 
