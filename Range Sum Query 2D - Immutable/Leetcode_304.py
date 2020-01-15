9f3e2883fe211c258e389b45daa15d2993310ce4
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-15 13:51:57 -0500

    added a more efficient DP solution

--- Range Sum Query 2D - Immutable/Leetcode_304.py
+++ Range Sum Query 2D - Immutable/Leetcode_304.py
@@ -1,3 +1,53 @@
+# a more efficient dynamic program
+# constant time query complexity
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        
+        self.dp = None
+        
+        def fill_dp():
+            """
+            Build-up memo table.
+            Time complexity: O(n*m)
+            """
+            if not (matrix and matrix[0]): return
+            else:
+                m, n = len(matrix), len(matrix[0])
+                self.dp = [[0]*(n+1) for _ in range(m+1)]
+                print(self.dp)
+                for row in range(m):
+                    for col in range(n):
+                        self.dp[row+1][col+1] = self.dp[row+1][col] + \
+                                                self.dp[row][col+1] + \
+                                                matrix[row][col] - \
+                                                self.dp[row][col]
+                            
+        fill_dp()
+                                
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        """
+        Query sum of elements inside rectangle with 
+        upper left hand corner (row1, col1) and
+        lower left hand corner (row2, col2)
+        
+        Time Complexity: O(1)
+        """
+        
+        if not self.dp: return 0
+        return self.dp[row2+1][col2+1] - self.dp[row1][col2+1] - \
+               self.dp[row2+1][col1] + self.dp[row1][col1]
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+**************************************************************
+
+
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
