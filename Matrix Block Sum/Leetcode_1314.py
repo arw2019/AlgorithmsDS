4406d38ec255b72a6ac5da3e20a0148a4799eb4d
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-16 08:28:14 -0500

    added faster numpy solution (with caching )

--- Matrix Block Sum/Leetcode_1314.py
+++ Matrix Block Sum/Leetcode_1314.py
@@ -1,5 +1,38 @@
+# numpy solution with caching 
+# AC, ~204ms
+import numpy as np
+
+class Solution:
+    def matrixBlockSum(self, matrix: List[List[int]], K: int) -> List[List[int]]:
+        
+        if not (matrix and matrix[0]): return [[]]
+        
+        m, n = len(matrix), len(matrix[0])
+        mat = np.array(matrix)
+        
+        for i in range(1,m):
+            for j in range(n):
+                mat[i,j] += mat[i-1,j]
+        for j in range(1,n):
+            for i in range(m):
+                mat[i,j] += mat[i,j-1]
+        
+        ans = [[0 for _ in range(n)] for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                lowerRow, upperRow = max(0, i-K), min(m-1, i+K)
+                lowerCol, upperCol = max(0, j-K), min(n-1, j+K)
+                sum1 = mat[upperRow, upperCol]
+                sum2 = mat[lowerRow-1, upperCol] if lowerRow>0 else 0
+                sum3 = mat[upperRow,lowerCol-1] if lowerCol>0 else 0
+                sum4 = mat[lowerRow-1, lowerCol-1] if (lowerRow>0 and lowerCol>0) else 0
+                ans[i][j] = sum1-sum2-sum3+sum4
+        
+        return ans
+
 # numpy solution - no caching
-# AC, top 80%
+# AC, ~360ms
 
 import numpy as np
 
