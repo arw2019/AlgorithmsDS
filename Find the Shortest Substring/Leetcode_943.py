47342d3ba57e6491d3e66adfdec992228d05dc84
arw2019 <andrew.r.wieteska@gmail.com>
2020-01-20 10:21:13 -0500

    travelling salesman problem; bfs solution

---
+++ Find the Shortest Substring/Leetcode_943.py
@@ -0,0 +1,59 @@
+# Leetcode Problem 943: Find the Shortest Substring 
+# credit: https://leetcode.com/problems/find-the-shortest-superstring/discuss/195487/python-bfs-solution-with-detailed-explanation(with-extra-Chinese-explanation)
+from collections import deque
+
+class Solution: 
+    def shortestSubstring(self, A: 'List[str]') -> str: 
+            
+        def getDistance(s1: str, s2: str) -> int:
+            """
+            Computes saved length when placing s2 
+            immediately behind s1
+            """
+            for i in range(1, len(s1)):
+                if s2.startswith(s1[i:]):
+                    return len(s1)-i
+            return 0
+
+        def pathToStr(path):
+            print(f'path={path}')
+            res = A[path[0]]
+            for i in range(1, len(path)):
+                res += A[path[i]][G[path[i-1]][path[i]]:]
+                print(G[path[i-1]][path[i]])
+                print(A[path[i]][G[path[i-1]][path[i]]:])
+            return res
+
+        n = len(A)
+        G = [[0]*n for _ in range(n)]
+        for i in range(n):
+            for j in range(i+1, n):
+                G[i][j] = getDistance(A[i], A[j])
+                G[j][i] = getDistance(A[j], A[i])
+
+        d = [[0]*n for _ in range(1<<n)]
+        Q = deque([(i, 1<<i, [i], 0) for i in range(n)]) 
+        l = -1
+        P = []
+        while Q:
+            node, mask, path, dis = Q.popleft()
+            if dis < d[mask][node]: continue
+            if mask == (1<<n) - 1 and dis > l:
+                P, l = path, dis
+                continue
+            for i in range(n):
+                next_mask = mask | (1<<i)
+                if next_mask != mask and d[mask][node] + G[node][i] >= d[next_mask][i]:
+                    d[next_mask][i] = d[mask][node] + G[node][i]
+                    Q.append((i, next_mask, path+[i], d[next_mask][i]))
+
+        return pathToStr(P)
+
+s = Solution()
+input_1 = ["alex","loves","leetcode"]
+result_1 = s.shortestSubstring(input_1)
+print(result_1)
+
+input_2 = ["catg","ctaagt","gcta","ttca","atgcatc"]
+result_2 = s.shortestSubstring(input_2)
+print(result_2)
