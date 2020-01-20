a5448d7145427a1225e1d1d95df7f0733a869a8f
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-20 10:12:23 -0500

    more concise implementation, same idea

--- Group the People Given the Group Size They Belong To/Leetcode_1282.py
+++ Group the People Given the Group Size They Belong To/Leetcode_1282.py
@@ -1,16 +1,15 @@
-from collections import defaultdict, Counter
+from collections import defaultdict
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         people_by_gs= defaultdict(list)
         for personId, groupSize in enumerate(groupSizes):
             people_by_gs[groupSize].append(personId)
         ans = []
-        for groupSize in set(groupSizes):
-            while people_by_gs[groupSize]:
+        for  groupSize, people in people_by_gs.items():
+            while people:
                 newGroup = []
-                curSize = people_by_gs[groupSize]
                 for _ in range(groupSize):
-                    newGroup.append(curSize.pop())
+                    newGroup.append(people.pop())
                 # print(f'new group: {newGroup}')
                 ans.append(newGroup)
         return ans
