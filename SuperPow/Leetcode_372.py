abf5500f9440c5cc12573ffc63d7ccb037e6083a
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-21 13:09:29 -0500

    a pythonic solution using in-built pow function

--- SuperPow/Leetcode_372.py
+++ SuperPow/Leetcode_372.py
@@ -1,3 +1,10 @@
+# solution using python's in-built pow function
+
+class Solution:
+    
+    def superPow(self, a: int, b: List[int]) -> int:
+        return pow(a, int(''.join(map(str, b))), 1337)
+
 # an efficient recursive solution (AC)
 
 class Solution:
