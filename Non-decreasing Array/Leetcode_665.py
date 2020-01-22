76146a98bc88a94447928162b42f826d0e8e1e12
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-22 13:44:26 -0500

    added alternative implementation of same idea

--- Non-decreasing Array/Leetcode_665.py
+++ Non-decreasing Array/Leetcode_665.py
@@ -1,3 +1,18 @@
+# alternative implementation of the same idea
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        cnt, pprev, prev = 0, float('-inf'), nums[0]
+        for num in nums[1:]:
+            cur = num
+            if prev > cur:
+                cnt +=1
+                if cnt==2: 
+                    return False
+                if pprev > cur: 
+                    cur = prev
+            pprev, prev = prev, cur
+        return True
+
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         cnt = 0
