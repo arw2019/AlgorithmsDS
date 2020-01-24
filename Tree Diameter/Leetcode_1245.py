2316f795eebcc9949e6178641c2d3a169ab63bea
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-01-24 14:24:41 -0500

    an efficient, two pass dfs solution

--- Tree Diameter/Leetcode_1245.py
+++ Tree Diameter/Leetcode_1245.py
@@ -1,3 +1,35 @@
+# an efficient dfs solution
+
+from collections import defaultdict
+
+class Solution:
+    def __init__(self):
+        self.best = [-1,-1]
+        self.adj = defaultdict(list)
+        
+    def treeDiameter(self, edges: List[List[int]]) -> int:
+
+        for u, v in edges:
+            self.adj[u] += [v]
+            self.adj[v] += [u]
+            
+        self.dfs(0, -1, 0)
+        self.dfs(self.best[1], -1, 0)
+        
+        return self.best[0]
+    
+    def dfs(self, u, parent, length):
+        if length > self.best[0]:
+            self.best = [length, u]
+        
+        for v in self.adj[u]:
+            if v != parent:
+                self.dfs(v, u, length+1)
+                
+                
+# dfs solution
+# too general - find max diameter of a connected component in a graph
+
 from collections import defaultdict
 
 class Solution:
