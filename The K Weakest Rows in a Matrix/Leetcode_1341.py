16018ac80bdecbbfadb4dfd55d23e9d1fdcf1df4
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-02-02 15:45:40 -0500

    linear time solution

--- The K Weakest Rows in a Matrix/Leetcode_1341.py
+++ The K Weakest Rows in a Matrix/Leetcode_1341.py
@@ -1,4 +1,27 @@
- # brute force solution O(N^2)  
+# linear time solution 
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        if not (mat and mat[0]) or k > len(mat): 
+            return []
+        m, n = len(mat), len(mat[0])
+        weakest_rows = []
+        col = 0
+        seen = set()
+        while col < n and len(weakest_rows) < k:
+            zeros = [i for i, row in enumerate(mat) 
+                     if row[col] == 0 and i not in seen]
+            seen |= set(zeros)       
+            weakest_rows += zeros
+            col += 1
+        i = 0
+        while len(weakest_rows) < k:
+                if i not in seen: weakest_rows.append(i)
+                i+=1 
+        return weakest_rows[:k]
+   
+
+# brute force solution O(N^2)  
  
 from collections import defaultdict
 class Solution:
