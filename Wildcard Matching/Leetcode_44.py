00a4b04e095c00e98fc55f39c0be86b5e0af81f3
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-02-07 15:00:35 -0500

    added a DP solution

--- Wildcard Matching/Leetcode_44.py
+++ Wildcard Matching/Leetcode_44.py
@@ -1,3 +1,23 @@
+# dynamic program
+# O(m*n) runtime
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        cur = [True] + [False]*(m)
+        print(cur)
+        for j in range(1, n+1):
+            pre = cur[0]
+            cur[0] = cur[0] and (p[j-1] == '*')
+            for i in range(1, m+1):
+                if p[j-1] != '*':
+                    pre, cur[i] = cur[i], pre and (s[i-1]==p[j-1] or p[j-1]=='?')
+                else:
+                    pre, cur[i] = cur[i], cur[i-1] or cur[i]
+        return cur[m]
+
+# finite-state machine solution
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         transfer = {}
