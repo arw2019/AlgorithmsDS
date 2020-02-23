cd6261ce863a36c0baca53af912264e80050325f
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-02-23 17:33:39 -0500

    added an o(n^2) solution for a square matrix

--- Spiral matrix/Leetcode_54.py
+++ Spiral matrix/Leetcode_54.py
@@ -13,3 +13,25 @@ class Solution:
             matrix = list(zip(*matrix))[::-1] 
         
         return ans
+
+# O(n^2) solution for a square matrix
+
+class Solution:
+    
+    def spiralOrder(self, square_matrix: List[List[int]]) -> List[int]:
+        
+        spiral_ordering: List[int] = []
+        
+        def matrix_layer_in_clockwise(offset):
+            if offset == len(square_matrix) - offset - 1:
+                spiral_ordering.append(square_matrix[offset][offset])
+            else:
+                spiral_ordering.extend(square_matrix[offset][offset:-1-offset])
+                spiral_ordering.extend(list(zip(*square_matrix))[-1-offset][offset:-1-offset])
+                spiral_ordering.extend(square_matrix[-1-offset][-1-offset:offset:-1])
+                spiral_ordering.extend(list(zip(*square_matrix))[offset][-1-offset:offset:-1])
+                
+        for offset in range((len(square_matrix)+1)//2):
+            matrix_layer_in_clockwise(offset)
+        
+        return spiral_ordering
