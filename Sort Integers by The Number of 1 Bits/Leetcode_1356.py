3e5e9b0e50bdc1816857a7d3c5fd720a5deb716a
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-02-26 11:02:20 -0500

    added a one-liner

--- Sort Integers by The Number of 1 Bits/Leetcode_1356.py
+++ Sort Integers by The Number of 1 Bits/Leetcode_1356.py
@@ -1,3 +1,9 @@
+# same algorithm, more concise code
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        return sorted(arr, key=lambda x: (bin(x)[2:].count('1'), x)) 
+
+# use python's in-built sort with a custom key
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
         number_of_1_bits = lambda x: bin(x)[2:].count('1') 
