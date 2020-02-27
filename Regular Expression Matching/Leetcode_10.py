0d8d84daec0e01e14eea344f3a9bc482eb5eb1ba
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-02-26 22:33:42 -0500

    improved readability

--- Regular Expression Matching/Leetcode_10.py
+++ Regular Expression Matching/Leetcode_10.py
@@ -1,3 +1,25 @@
+# a more readable version of the same algorithm
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p: return not s
+        
+        dp = [[False]*(len(p)+1) for _ in range(len(s)+1) ]
+        
+        dp[0][0] = True
+        for j in range(2, len(p)+1):
+            dp[0][j] = (p[j-1]=='*') and dp[0][j-2]
+        
+        
+        for j in range(1, len(p)+1):
+            for i in range(1, len(s)+1):
+                if p[j-1] in (s[i-1], '.'):
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-2] or (p[j-2] in ('.', s[i-1]) and dp[i-1][j])
+        
+        return dp[len(s)][len(p)]
+
 # dynamic program
 # O(nm) runtime
 class Solution:
