2a8f35aa19ef65f51014dc9d3493f04d455fcfe5
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-02-26 21:30:37 -0500

    added another implementation of the same idea

--- Min Stack/Leetcode_155.py
+++ Min Stack/Leetcode_155.py
@@ -1,3 +1,5 @@
+# version 1
+
 from collections import deque
 class MinStack:
 
@@ -21,6 +23,36 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.stack[-1][1] if self.stack else None
+
+ # version 2
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.smallestElements = [float('inf')]
+
+    def push(self, x: int) -> None:
+        self.stack += [x]
+        self.smallestElements += [min(self.smallestElements[-1], x)]
+
+    def pop(self) -> None:
+        self.stack.pop()   
+        self.smallestElements.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.smallestElements[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
 
 
 # Your MinStack object will be instantiated and called as such:
