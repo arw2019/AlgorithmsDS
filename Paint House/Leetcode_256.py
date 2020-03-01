ae73008c286206ccbb57d198db15033560b97675
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-03-01 17:51:21 -0500

    optimized DP solution from linear to constant space

--- Paint House/Leetcode_256.py
+++ Paint House/Leetcode_256.py
@@ -1,3 +1,19 @@
+# same DP solution
+# but optimized to constant space
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        if not costs or not costs[0]: return 0
+        cost0, cost1, cost2 = 0, 0, 0
+        for row in costs:
+            cost0, cost1, cost2 = \
+            row[0] + min(cost1, cost2), \
+            row[1] + min(cost0, cost2), \
+            row[2] + min(cost0, cost1)
+        return min(cost0, cost1, cost2)
+
+# DP solution
+# O(N) time, O(N) space
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
         if not costs or not costs[0]: return 0
