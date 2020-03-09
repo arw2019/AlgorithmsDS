9d4ecfa3e853151613e62f912c5af09580f27d33
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-03-08 23:25:29 -0400

    binary search solution, O(NlgN) time

--- Longest Increasing Subsequence/Leetcode_300.py
+++ Longest Increasing Subsequence/Leetcode_300.py
@@ -1,3 +1,19 @@
+# O(NlgN) solution
+# O(L) space where L is the length of LIS
+# so also can say O(N) space
+
+from bisect import bisect_left
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        ISS = []
+        for n in nums:
+            insertPos = bisect_left(ISS, n)
+            if insertPos == len(ISS):
+                ISS += [n]
+            else:
+                ISS[insertPos] = n
+        return len(ISS)
+
 # bottom-up DP
 # O(N^2) time, O(N) space
 
