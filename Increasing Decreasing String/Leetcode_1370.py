ffe6993612879acfbc93c152844f69dd170fc10d
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-03-10 22:14:52 -0400

    same algo, simpler code

--- Increasing Decreasing String/Leetcode_1370.py
+++ Increasing Decreasing String/Leetcode_1370.py
@@ -4,27 +4,23 @@ class Solution:
         cnt = Counter(s)
         chars= sorted(cnt.keys())
         res = []
+        
         while len(res) < len(s):
             i=0
             while i<len(chars) and cnt[chars[i]] == 0: i+=1
-            if cnt[chars[i]] > 0: 
-                res += [chars[i]]
-                cnt[chars[i]] -= 1
             
-            for char in chars[i+1:]:
+            for char in chars[i:]:
                 if cnt[char] > 0:
                     res += [char]
                     cnt[char] -= 1
                     
             i = len(chars)-1
             while i>0 and cnt[chars[i]] == 0: i-=1
-            if cnt[chars[i]] > 0: 
-                res += [chars[i]]
-                cnt[chars[i]] -= 1
             
-            for char in reversed(chars[:i]):
+            for char in reversed(chars[:i+1]):
                 if cnt[char] > 0:
                     res += [char]
                     cnt[char] -= 1
       
         return ''.join(res)
+    
