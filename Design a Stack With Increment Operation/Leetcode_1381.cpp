e955f598e288767eec393f8183cc7e8d881ffeb0
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-04-28 16:38:20 -0400

    added a second implementation of the same idea

--- Design a Stack With Increment Operation/Leetcode_1381.cpp
+++ Design a Stack With Increment Operation/Leetcode_1381.cpp
@@ -37,6 +37,65 @@ public:
     }
 };
 
+
+
+// another version
+
+class CustomStack {
+    private:
+    int *stack;
+    int head;
+    int tail;
+    int elem;
+    int mx;
+public:
+    CustomStack(int maxSize) {
+        stack = (int *) malloc(maxSize*sizeof(int));
+        head = 0;
+        tail = 0;
+        elem = 0;
+        mx = maxSize;
+    }
+    
+    void push(int x) {
+        if (elem < mx){
+            stack[head] = x;
+            elem++; 
+            head++;
+        }
+    }
+    
+    int pop() {
+        if (elem==0){
+            return -1;
+        }
+        else{
+            head--;
+            elem--;
+            return stack[head];
+        }
+    }
+    
+    void increment(int k, int val) {
+        if (k>elem){
+            for (int i=elem-1; i>=0; i--){
+                stack[i] += val;
+            }
+        } 
+        else {
+            for (int i=k-1; i>=0; i--){
+                stack[i] += val;
+            }
+        }
+    }
+};
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
 /**
  * Your CustomStack object will be instantiated and called as such:
  * CustomStack* obj = new CustomStack(maxSize);
