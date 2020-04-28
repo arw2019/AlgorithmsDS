92e5892cdc4fd01fbad3d9edd366a40497871614
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-04-27 23:53:26 -0400

    AC solution

--- Design a Stack With Increment Operation/Leetcode_1381.py
+++ Design a Stack With Increment Operation/Leetcode_1381.py
@@ -6,26 +6,26 @@ class CustomStack:
         self.n = 0
 
     def push(self, x: int) -> None:
-        if self.n < self.maxSize - 1:
+        # print(f"pushing {x}, stack={self.stack}")
+        if self.n < self.maxSize:
             self.stack[self.n] = x
             self.n += 1
-        print(self.stack)
+        # print(f"stack following push: {self.stack}")
 
     def pop(self) -> int:
         if self.n > 0:
             value = self.stack[self.n-1]
-            self.stack[self.n] = None
+            self.stack[self.n-1] = None
             self.n -= 1
         else:
             value = -1
         return value
 
     def increment(self, k: int, val: int) -> None:
-        i=0
-        while i < k and i < self.n:
+        numElements = min(k, self.n)
+        for i in range(numElements):
             self.stack[i] += val
-            i += 1
-        print(self.stack)
+        # print(self.stack)
 
 
 # Your CustomStack object will be instantiated and called as such:
