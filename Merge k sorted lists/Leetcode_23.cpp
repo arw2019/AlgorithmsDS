00cce5d82a8c2933405b680221ad5272a1262040
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-01 17:34:24 -0400

    added another implementation of algo

--- Merge k sorted lists/Leetcode_23.cpp
+++ Merge k sorted lists/Leetcode_23.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -37,5 +27,31 @@ public:
         }
         
         return dummy->next;
+    }
+};
+
+// same idea, another implementation. Slightly cleaner IMO
+
+class Solution {
+public:
+    struct CompareListNode{
+      bool operator()(ListNode* node1, ListNode* node2) {return node1->val > node2->val;}  
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, CompareListNode> pq;
+        
+        for (ListNode* node: lists) {if (node != NULL) {pq.push(node);}}
+        
+        ListNode* dummy_head = new ListNode(INT_MIN);
+        ListNode *cur = dummy_head;
+        
+        while(!pq.empty()){
+            cur->next = pq.top();
+            pq.pop();
+            if (cur->next->next != NULL) {pq.push(cur->next->next); }
+            cur = cur->next;
+        }
+        return dummy_head->next;
     }
 };
