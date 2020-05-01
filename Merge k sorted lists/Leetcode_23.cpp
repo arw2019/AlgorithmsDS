92d18b571f6e6ec56693e6d2844e255399c642ce
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-01 17:34:49 -0400

    added definition of singly-linked list for reference

--- Merge k sorted lists/Leetcode_23.cpp
+++ Merge k sorted lists/Leetcode_23.cpp
@@ -55,3 +55,14 @@ public:
         return dummy_head->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
