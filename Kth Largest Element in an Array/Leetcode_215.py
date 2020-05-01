5bf62070d8350907506c616f3e659245f4c4e788
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-01 17:57:33 -0400

    added explicit implementation of heapq.nlargest + notes

--- Kth Largest Element in an Array/Leetcode_215.py
+++ Kth Largest Element in an Array/Leetcode_215.py
@@ -1,3 +1,20 @@
+# an implementation of n-largest using heapq's basic methods
+# time complexity: O(NlgN), space complexity: O(N)
+
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        pq = []
+        for n in nums:
+            heapq.heappush(pq, -n)
+        for _ in range(k-1):
+            heapq.heappop(pq)
+        return -heapq.heappop(pq)
+    
+# solution using heapq library's built-in function
+# equivalent to sorted(nums)[:n] 
+# time complexity: O(NlgN), space complexity: O(N)
+
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
