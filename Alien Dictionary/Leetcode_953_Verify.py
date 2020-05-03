fb661bf006b788e94d053a024359bde3ff900f9b
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-03 18:57:45 -0400

    added an implementation with early stopping

--- Alien Dictionary/Leetcode_953_Verify.py
+++ Alien Dictionary/Leetcode_953_Verify.py
@@ -1,10 +1,11 @@
+# same algo with early stopping
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        aliendict = {letter: priority for priority, letter in enumerate(order)}
-        english = list("abcdefghijklmnopqrstuvwxyz") 
-        def toEnglish(word: str):
-            return ''.join([english[aliendict[char]] for char in word])
-        return sorted(words, key=lambda w: toEnglish(w)) == words
+        d = {char: i for i, char in enumerate(order)}
+        def isGreater(w1: str, w2: str) -> bool:
+            key = lambda word: [d[char] for char in word]
+            return key(w1) > key(w2)
+        return all(isGreater(words[i+1], words[i]) for i in range(len(words)-1))
 
 # lighter implementation of same idea
 class Solution:
@@ -12,3 +13,11 @@ class Solution:
         d = {char: i for i, char in enumerate(order)}
         wordsAlienOrder = sorted(words, key=lambda word: [d[char] for char in word]) 
         return words == wordsAlienOrder
+    
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        aliendict = {letter: priority for priority, letter in enumerate(order)}
+        english = list("abcdefghijklmnopqrstuvwxyz") 
+        def toEnglish(word: str):
+            return ''.join([english[aliendict[char]] for char in word])
+        return sorted(words, key=lambda w: toEnglish(w)) == words
