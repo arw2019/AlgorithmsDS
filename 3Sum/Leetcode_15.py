2597966167e5ef394b7f582d7a96256eab5f50d5
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-03 15:58:00 -0400

    a cleaner implementation of same algorithm

--- 3Sum/Leetcode_15.py
+++ 3Sum/Leetcode_15.py
@@ -1,81 +1,23 @@
-# slighly faster - cuts down search space & handles some edge cases by hand
-class Solution:
-    def threeSum(self, A: List[int]) -> List[List[int]]:
-        if not A or len(A) < 3: return []
-        A.sort()
-        if A[0] == A[-1] == 0: return [[0,0,0]]
-        if A[0] >= 0 or A[-1] <= 0: return []
-        res = []
-        non_zero_index = next(index for index, val in enumerate(A) if val > 0)
-        for pivot in range(non_zero_index):
-            # skip if already computed
-            if pivot > 0 and A[pivot-1] == A[pivot]:
-                continue
-            left = pivot+1
-            right = len(A)-1
-            while left < right:
-                tot = A[pivot] + A[left] + A[right]
-                if tot > 0:
-                    while left < right and A[right] == A[right-1]:
-                        right -= 1
-                    right -=1
-                elif tot < 0:
-                    while left < right and A[left] == A[left+1]:
-                        left += 1
-                    left += 1
-                else:
-                    res.append([A[pivot], A[left], A[right]])
-                    while left < right and A[right] == A[right-1]:
-                        right -= 1
-                    while left < right and A[left] == A[left+1]:
-                        left += 1
-                    right -= 1
-                    left += 1
-        return res
+# top 25%
 
-#------------------------------------------------------------------
-    
-class Solution:
-    def threeSum(self, A: List[int]) -> List[List[int]]:
-        A.sort()
-        res = []
-        for i in range(len(A)-2):
-            if i == 0 or (i > 0 and A[i]!=A[i-1]):
-                lo, hi, tot = i+1, len(A)-1, 0-A[i]
-                while lo < hi:
-                    if A[lo] + A[hi] == tot:
-                        res += [[A[i], A[lo], A[hi]]]
-                        while lo < hi and A[lo] == A[lo+1]: lo += 1
-                        while lo < hi and A[hi-1] == A[hi]: hi -= 1
-                        lo += 1
-                        hi -= 1 
-                    elif A[lo] + A[hi] < tot:
-                        while lo < hi and A[lo] == A[lo+1]: lo += 1
-                        lo += 1
-                    else:
-                        while lo < hi and A[hi-1] == A[hi]: hi -= 1
-                        hi -= 1
-        return res
-
-#-----------------------------------
-# hashtable solution
-# O(N^2) runtime, O(N) space
-
-from collections import Counter
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        cnt = Counter(nums)
-        uniqueNums = list(sorted(cnt.keys()))
-        res = []
-        for i, a in enumerate(uniqueNums):
-            for j, b in enumerate(uniqueNums[i:], i):
-                c = - (a + b)
-                if a == b == c:
-                    if cnt[a] >= 3: res+=[[a,a,a]]
-                elif a == b:
-                    if cnt[a]>=2 and cnt[c] > 0: res += [[a, a, c]]
-                elif c == a or c == b:
-                    if cnt[c] >=2: res += [[a, b, c]]
-                else:
-                    if cnt[c] > 0: res += [[a, b, c]]
-        return list(set(tuple(sorted(triplet)) for triplet in res))
+        ht = collections.defaultdict(int)
+        for i, n in enumerate(nums):
+            ht[n] += 1
+        nums = sorted(list(ht.keys()))
+        N = len(nums)
+        res = set()
+        for i in range(N):
+            n1 = nums[i]
+            for j in range(i+1, N):
+                n2 = nums[j]
+                n3 = - (n1+n2)
+                if (n3==n1 or n3==n2):
+                    if ht[n3]>= 2: res.add(tuple(sorted([n1, n3, n2])))
+                elif n3 in ht:
+                     res.add(tuple(sorted([n1, n3, n2])))
+        if 0 in ht and ht[0] >= 3:
+            res.add((0, 0, 0))
+        
+        return res
