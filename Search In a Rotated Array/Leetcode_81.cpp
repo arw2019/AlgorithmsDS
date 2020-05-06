9e23b3676c5d2b258e8ca64f5b0e3cb5e9309b41
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-06 11:59:09 -0400

    added line to deal with dups of pivot

--- Search In a Rotated Array/Leetcode_81.cpp
+++ Search In a Rotated Array/Leetcode_81.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.size()==0) return -1;
-        int lo=0, hi=nums.size()-1, mid;
+        if (nums.size()==0) return false;
+        int lo=0, hi=nums.size()-1;
+        while (hi>0 && nums[hi]==nums[0]) hi--;
+        int mid;
         bool b_target=nums[0]>target, b_mid;
         while (lo<=hi){
             mid = lo + (hi-lo)/2;
             b_mid = nums[0] > nums[mid]; 
-            if (nums[mid] == target) return mid;
+            if (nums[mid] == target) return true;
             if (b_mid<b_target || (b_mid == b_target && nums[mid]< target)) {
                 lo = mid+1;
             } else hi = mid-1;
         }
-        return -1;
+        return false;
     }
 };
