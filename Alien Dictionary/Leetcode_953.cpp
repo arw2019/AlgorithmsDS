5a094e9581d1f45f85e1cb36c4270686c65e16d5
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-05 23:29:22 -0400

    completed isAlienSorted

--- Alien Dictionary/Leetcode_953.cpp
+++ Alien Dictionary/Leetcode_953.cpp
@@ -11,15 +11,16 @@ public:
     }
     
     bool isGreater(string word1, string word2){
-        return toAlien(word1) >= toAlien(word2);
+        return toAlien(word2) >= toAlien(word2);
     }
     
     bool isAlienSorted(vector<string>& words, string order) {
         for (int i=0; i<order.size(); ++i){
             aliendict[order[i]] = 'a' + i;
-            cout << order[i] << " " << aliendict[order[i]] << endl;
         }
-        // cout << toAlien("hla"); 
-        return false;
+        for (int j=0; j<words.size()-1; j++){
+            if (!isGreater(words[j+1], words[j])) return false;
+        }
+        return true;
     }
 };
