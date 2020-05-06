288c5b0a7f0dffebffa37ad89cde16f9d3093793
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-05 23:26:59 -0400

    function to compare strings in alien

--- Alien Dictionary/Leetcode_953.cpp
+++ Alien Dictionary/Leetcode_953.cpp
@@ -10,6 +10,10 @@ public:
         return alien; 
     }
     
+    bool isGreater(string word1, string word2){
+        return toAlien(word1) >= toAlien(word2);
+    }
+    
     bool isAlienSorted(vector<string>& words, string order) {
         for (int i=0; i<order.size(); ++i){
             aliendict[order[i]] = 'a' + i;
