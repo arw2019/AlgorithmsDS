f64d3b9ef8ee62c446616607ea660c851213fa9f
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-06 11:53:19 -0400

    fixed loop termination condition

--- Search In a Rotated Array/Leetcode_81.cpp
+++ Search In a Rotated Array/Leetcode_81.cpp
@@ -3,11 +3,12 @@ public:
     int search(vector<int>& nums, int target) {
         if (nums.size()==0) return -1;
         int lo=0, hi=nums.size()-1, mid;
+        bool b_target=nums[0]>target, b_mid;
         while (lo<=hi){
             mid = lo + (hi-lo)/2;
-            cout<<mid<<" "<<nums[mid]<<endl;
+            b_mid = nums[0] > nums[mid]; 
             if (nums[mid] == target) return mid;
-            if (((nums[0]<nums[mid]) && (nums[0]>target)) || (nums[mid] < target)) {
+            if (b_mid<b_target || (b_mid == b_target && nums[mid]< target)) {
                 lo = mid+1;
             } else hi = mid-1;
         }
