30f0cde034fe77ce5e99cfb1584f9974c199680b
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-05 23:43:53 -0400

    added alternative implementation w/o map

--- Alien Dictionary/Leetcode_953.cpp
+++ Alien Dictionary/Leetcode_953.cpp
@@ -24,3 +24,34 @@ public:
         return true;
     }
 };
+
+// alternative solution
+// doesn't use an associative container
+
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<char> indicies(26);
+        for (int i=0; i<order.size(); i++){
+            indicies[order[i]%26] = i;
+        }
+        
+        for (int i=0; i<words.size()-1; i++){
+            string & word1 = words[i];
+            string & word2 = words[i+1];
+            int len1 = word1.size();
+            int len2 = word2.size();
+            bool same = true;
+            for (int j=0; j<min(len1, len2); j++){
+                if (indicies[word1[j]%26]> indicies[word2[j]%26]){
+                    return false;
+                } else if (indicies[word1[j]%26] < indicies[word2[j]%26]){
+                    same = false;
+                    break;
+                }
+                if (same && len1 > len2) return false;
+            }
+        }
+        return true;
+    }
+};
