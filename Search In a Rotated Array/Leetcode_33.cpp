59d022f1824a945de585261179e722a1896e9289
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-05 23:59:38 -0400

    removed debug statements

--- Search In a Rotated Array/Leetcode_33.cpp
+++ Search In a Rotated Array/Leetcode_33.cpp
@@ -5,7 +5,6 @@ public:
         int lo=0, hi=nums.size()-1, mid;
         while (lo<=hi){
             mid = lo + (hi-lo)/2;
-            cout<<mid<<" "<<nums[mid]<<endl;
             if (nums[mid] == target) return mid;
             if (((nums[0]<nums[mid]) && (nums[0]>target)) || (nums[mid] < target)) {
                 lo = mid+1;
