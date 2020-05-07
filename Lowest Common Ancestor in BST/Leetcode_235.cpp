fe3125fd0fd2be54b79fb3aaffe70604aebadd69
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-06 22:35:00 -0400

    added recursive solution

--- Lowest Common Ancestor in BST/Leetcode_235.cpp
+++ Lowest Common Ancestor in BST/Leetcode_235.cpp
@@ -7,6 +7,21 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val) {
+            TreeNode* tmp = p;
+            p = q;
+            q = tmp;
+        }
+        if (root->val < p->val) return lowestCommonAncestor(root->right, p, q);
+        else if (root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+        else return root;
+    }
+};
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
