21ab8f30ec4e5c153bbabfd90c483b9f799561b9
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-06 22:11:24 -0400

    O(h) time complexity

---
+++ Lowest Common Ancestor in Binary Tree/Leetcode_236.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val) {
+            TreeNode* tmp = p;
+            p = q;
+            q = tmp;
+        }
+        while (root->val < p->val && root->val > q->val){
+            cout << root->val << endl;
+            while(root->val < p->val) {root=root->right; cout << root->val << endl;}
+            while(root->val > q->val) {root=root->left; cout << root->val << endl;}
+        }
+        return root;
+    }
+};
