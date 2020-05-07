2b59bcc7a277a15f015d29697b7d00c005a4363c
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-06 22:26:46 -0400

    added recursive solution

--- Lowest Common Ancestor in BST/Leetcode_235.py
+++ Lowest Common Ancestor in BST/Leetcode_235.py
@@ -7,6 +7,19 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > q.val: 
+            p, q = q, p
+            
+        if p.val > root.val: 
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif q.val < root.val: 
+            return self.lowestCommonAncestor(root.left, p, q)
+        else: 
+            return root
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if p.val > q.val: p, q = q, p
         while p.val>root.val or q.val<root.val:
             while p.val>root.val: root = root.right
