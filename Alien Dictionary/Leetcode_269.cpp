f860bd9965e970ba096ecf03a0858d7b6f16b7a9
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-10 18:48:44 -0400

    an (almost) working solution

--- Alien Dictionary/Leetcode_269.cpp
+++ Alien Dictionary/Leetcode_269.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
-        vector <pair<char, char>> less;
+        vector <pair<char, char>> less, tmp;
         pair<char, char> p;
         for (int i=0; i<words.size()-1; ++i){
             string word1=words[i], word2=words[i+1];
@@ -15,7 +15,48 @@ public:
                 }
             }
         }
+        
+        for (auto p: less) cout<<p.first<<" "<<p.second<<endl;
+        
+        set<char> chars;
+        for (int i=0; i<words.size(); ++i){
+            for (int j=0; j<words.size()-1; ++j) {
+                cout << "i: "<<i<< " j: "<<j << " char: "<<words[i][j] << endl;
+                chars.insert(words[i][j]);
+            }
+        }
 
-        return "";
+       
+        for (auto it=chars.begin(); it!=chars.end(); ++it) cout<<"*it: "<<*it<<endl;
+        
+        
+        string order="";
+        if (less.size() == 0){
+            for (int i=0; i<words.size()-1; ++i) {
+                if (words[i+1].size() < words[i].size()) return "";
+            }
+        }
+       
+        while (less.size() > 0){
+            set<char> free = chars;
+            for (int i=0; i<less.size(); ++i){
+                free.erase(less[i].second); 
+            }
+            for (auto it=free.begin(); it!=free.end(); ++it) cout<<"*it: "<<*it<<endl;
+            for (auto it=free.begin(); it!=free.end(); ++it) {
+                order+=*it;
+                chars.erase(*it);
+            }
+            tmp.clear();
+            for (auto it=less.begin(); it<less.end(); it++){
+                pair<char, char> p = *it;
+                if (!chars.count(p.first) && !chars.count(p.second)) tmp.push_back(p);
+            }
+            less = tmp;
+        }
+        
+        for (auto it=chars.begin(); it!=chars.end(); it++) order += *it;
+
+        return order;
     }
 };
