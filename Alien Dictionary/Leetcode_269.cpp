666e9495e4dc86ee9f83e07a9fb4e62602b41b92
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-10 18:02:35 -0400

    rewrote graph build with pair<char, char>

--- Alien Dictionary/Leetcode_269.cpp
+++ Alien Dictionary/Leetcode_269.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
-        vector <string> less;
+        vector <pair<char, char>> less;
+        pair<char, char> p;
         for (int i=0; i<words.size()-1; ++i){
             string word1=words[i], word2=words[i+1];
             for (int j=0; j<min(word1.size(), word2.size()); ++j){
                 char char1=word1[j], char2=word2[j];
                 if (char1 != char2){
-                    string pair = string() + char1+char2; cout<<"pair: " <<pair<<endl;
-                    less.push_back(pair);
+                    p= make_pair(char1, char2); 
+                    cout<<"pair: " << p.first << " " << p.second<<endl;
+                    less.push_back(p);
                     break;
                 }
             }
         }
-        
-        set<char> chars;
-        for (int i=0; i<words.size(); i++){
-            for (int j=0; j<words.size(); ++j) chars.insert(words[i][j]);
-        }
-       
-        
+
         return "";
     }
 };
