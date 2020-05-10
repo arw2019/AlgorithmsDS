64981b82ac32cf05e6eeeccf262b1df7f5e79d96
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-10 13:59:58 -0400

    built up graph

---
+++ Alien Dictionary/Leetcode_269.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        vector <string> less;
+        for (int i=0; i<words.size()-1; ++i){
+            string word1=words[i], word2=words[i+1];
+            for (int j=0; j<min(word1.size(), word2.size()); ++j){
+                char char1=word1[j], char2=word2[j];
+                if (char1 != char2){
+                    string pair = string() + char1+char2; cout<<"pair: " <<pair<<endl;
+                    less.push_back(pair);
+                    break;
+                }
+            }
+        }
+        
+        return "";
+    }
+};
