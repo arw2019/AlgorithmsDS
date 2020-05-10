94b34fae0ca490c8e6e76d84dbc0faabae0bf82f
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-10 19:15:23 -0400

    A cleaned-up version
    Removed debug statements

--- Alien Dictionary/Leetcode_269.cpp
+++ Alien Dictionary/Leetcode_269.cpp
@@ -8,20 +8,17 @@ public:
             for (int j=0; j<min(word1.size(), word2.size()); ++j){
                 char char1=word1[j], char2=word2[j];
                 if (char1 != char2){
-                    p= make_pair(char1, char2); 
-                    cout<<"pair: " << p.first << " " << p.second<<endl;
+                    p = make_pair(char1, char2); 
                     less.push_back(p);
                     break;
                 }
             }
         }
         
-        for (auto p: less) cout<<p.first<<" "<<p.second<<endl;
         
         set<char> chars, free;
         for (int i=0; i<words.size(); ++i){
             for (int j=0; j<words[i].size(); j++) {
-                cout << "i: "<<i<< " j: "<<j << " char: "<<words[i][j] << endl;
                 chars.insert(words[i][j]);
             }
         }
@@ -38,31 +35,19 @@ public:
             free = chars;
             for (int i=0; i<less.size(); ++i){
                 free.erase(less[i].second); 
-                cout << "erasing from free: " <<less[i].second << endl; 
             }
             if (free.size()==0) return ""; 
-            cout<<"Free: "<< endl;
             for (auto it=free.begin(); it!=free.end(); ++it) { 
-                cout<<"*it: "<<*it<<endl;
                 order+=*it;
                 chars.erase(*it);
             }
             
-            cout <<"This is chars: "<<endl;
-            for (auto it=chars.begin(); it!=chars.end(); ++it) cout<<"*it: "<<*it<<endl;
-            
-            tmp.clear(); cout << "New less: " << endl;
+            tmp.clear(); 
             for (auto it=less.begin(); it<less.end(); it++){
                 p = *it;
-                cout<<p.first<< " " << p.second << endl;
-                if (free.count(p.first)==0 && free.count(p.second)==0){
-                    cout << "adding to tmp" << endl;
-                    tmp.push_back(p);
-                }
+                if (free.count(p.first)==0 && free.count(p.second)==0) tmp.push_back(p);
             }
             less = tmp;
-            cout << "tmp size: " << tmp.size() << endl;
-            cout << "less size: " << less.size() << endl;
         }
         
         for (auto it=chars.begin(); it!=chars.end(); it++) order += *it;
