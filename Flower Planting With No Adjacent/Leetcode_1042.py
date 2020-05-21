4bc3d80bd6b3bee11382a776ef178512de5ea2ba
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-21 14:44:33 -0400

    simplify processing step

--- Flower Planting With No Adjacent/Leetcode_1042.py
+++ Flower Planting With No Adjacent/Leetcode_1042.py
@@ -1,19 +1,12 @@
 class Solution:
     def gardenNoAdj(self, N: int, paths: List[List[int]]) -> List[int]:
-        allColors = {1, 2, 3, 4}
         g = defaultdict(list)
-        degrees = defaultdict(list)
         for u, v in paths:
             g[u] += [v]
             g[v] += [u]
-        for u, neighbours in g.items():
-            degrees[len(neighbours)] += [u]
-        colors = [None]*(1+N)
-        for deg in (3, 2, 1):
-            for u in degrees[deg]:
-                if colors[u]: continue
-                available = allColors - {colors[v] for v in g[u] if colors[v]}
-                colors[u] = available.pop()
-        for i in range(1, N+1):
-            if not colors[i]: colors[i]=1
+        colors = [None]*(N+1)
+        for u in range(1, N+1):
+            if colors[u]: continue
+            available = {1,2,3,4} - {colors[v] for v in g[u] if colors[v]}
+            colors[u] = available.pop()
         return colors[1:]
