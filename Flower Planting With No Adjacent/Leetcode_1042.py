9db04ce122d313ebc6bb38b2da0bf1e5b2e96602
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-21 14:47:03 -0400

    Fix array indexing

--- Flower Planting With No Adjacent/Leetcode_1042.py
+++ Flower Planting With No Adjacent/Leetcode_1042.py
@@ -2,11 +2,11 @@ class Solution:
     def gardenNoAdj(self, N: int, paths: List[List[int]]) -> List[int]:
         g = defaultdict(list)
         for u, v in paths:
-            g[u] += [v]
-            g[v] += [u]
-        colors = [None]*(N+1)
-        for u in range(1, N+1):
-            if colors[u]: continue
-            available = {1,2,3,4} - {colors[v] for v in g[u] if colors[v]}
-            colors[u] = available.pop()
-        return colors[1:]
+            g[u-1] += [v-1]
+            g[v-1] += [u-1]
+        colors = [None]*N
+        for u in range(N):
+            if not colors[u]:
+                available = {1,2,3,4} - {colors[v] for v in g[u] if colors[v]}
+                colors[u] = available.pop()
+        return colors
