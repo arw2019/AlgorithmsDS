6fdb36eab864051986d518b0632c4c767dac4132
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-21 18:13:35 -0400

    added memoization

--- Partition Array for Maximum Sum/Leetcode_1043.py
+++ Partition Array for Maximum Sum/Leetcode_1043.py
@@ -2,10 +2,13 @@ class Solution:
     def maxSumAfterPartitioning(self, A: List[int], K: int) -> int:
         N = len(A)
         dp = [0]*(N+1)
+        maxVal = {} 
         for i in range(N+1):
             for j in range(1, K+1):
                 if j<=i:
-                    newVal = dp[i-j] + j * max((A[m] for m in range(i-j, i)), default=0)
+                    if (i,j) not in maxVal: 
+                        maxVal[i,j] = max((A[m] for m in range(i-j, i)))
+                    newVal = dp[i-j] + j * maxVal[i,j]
                     # print(newVal)
                     if newVal > dp[i]: dp[i] = newVal
             # print(dp)
