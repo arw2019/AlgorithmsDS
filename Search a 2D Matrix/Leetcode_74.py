1aa2b928730953058fbcecebf8e781dfa93467d5
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-28 14:24:43 -0400

    removed debug statements

--- Search a 2D Matrix/Leetcode_74.py
+++ Search a 2D Matrix/Leetcode_74.py
@@ -4,8 +4,6 @@ class Solution:
         firstCol = [row[0] for row in matrix] 
         i = bisect.bisect_right(firstCol, target)-1
         if i < 0: return False
-        print(f"i={i}, {matrix[i]}")
         j = bisect.bisect_right(matrix[i], target) -1
         if j < 0: return False
-        print(f"j={j}, {matrix[i][j]}")
         return matrix[i][j] == target
