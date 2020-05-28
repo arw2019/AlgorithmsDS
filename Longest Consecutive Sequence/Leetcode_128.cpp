ef620b1dc941e77d44b60b7ad2b9c055b5b909e9
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-27 23:55:58 -0400

    used long ints to handle overflow

--- Longest Consecutive Sequence/Leetcode_128.cpp
+++ Longest Consecutive Sequence/Leetcode_128.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.size() == 0) return 0; 
-        unordered_set<int> unique_nums;
-        int best = 1, num;
+        unordered_set<long int> unique_nums;
+        long int num;
+        int best = 1;
         for (int i=0; i<nums.size(); ++i) unique_nums.insert(nums[i]);
         for (auto it=unique_nums.begin(); it!=unique_nums.end(); it++){
             if (unique_nums.count(*it-1) == 0){
                 num = *it + 1;
                 while(unique_nums.count(num++))
-                best = max(best, num - *it);
+                if ((num - *it) > best) best = num - *it;
             }
         }
         return best;
