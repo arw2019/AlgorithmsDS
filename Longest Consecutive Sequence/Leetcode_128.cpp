48e33bb8f3e96d6745b38a4e1066e0fe8f648008
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-27 23:53:31 -0400

    handled empty input

--- Longest Consecutive Sequence/Leetcode_128.cpp
+++ Longest Consecutive Sequence/Leetcode_128.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0) return 0; 
         unordered_set<int> unique_nums;
         int best = 1, num;
         for (int i=0; i<nums.size(); ++i) unique_nums.insert(nums[i]);
         for (auto it=unique_nums.begin(); it!=unique_nums.end(); it++){
             if (unique_nums.count(*it-1) == 0){
-                cout << *it << "  is the start of a sequence" << endl;
                 num = *it + 1;
                 while(unique_nums.count(num++))
                 best = max(best, num - *it);
