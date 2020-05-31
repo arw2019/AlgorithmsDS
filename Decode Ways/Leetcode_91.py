f4adc4fa6de178e1ca12c225ab676962b70aae7c
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-05-31 14:10:00 -0400

    top-down DP

---
+++ Decode Ways/Leetcode_91.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+class Solution:
+    @lru_cache()
+    def numDecodings(self, s: str) -> int:
+        # print(f"Calling numDecodings with {s}.")
+        if not s or s[0]=="0": return 0
+        if len(s)==1: return 1
+        if int(s[0:2]) > 26:
+            ans = self.numDecodings(s[1:])
+            # print(f"ans={ans}")
+            return ans
+        else:
+            ans = self.numDecodings(s[1:]) + (self.numDecodings(s[2:]) if len(s)>2 else 1)
+            # print(f"ans={ans}")
+            return ans
+        
