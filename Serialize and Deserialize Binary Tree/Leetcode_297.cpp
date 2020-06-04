7a19f8ebfa3d56981888ebe1aec4058f94ca2d28
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-06-03 23:26:42 -0400

    AC solution; use preorder traversal with tags

--- Serialize and Deserialize Binary Tree/Leetcode_297.cpp
+++ Serialize and Deserialize Binary Tree/Leetcode_297.cpp
@@ -9,28 +9,50 @@
  */
 class Codec {
 private:
-    string vals;
+    string wstr;
+    vector <string> rvec; 
+    int i;
 
 public:
     void write(TreeNode* node){
-        if (node == NULL) vals+= " #";
+        if (node == NULL) wstr+= " #";
         else{
-            vals += " " + to_string(node->val);
-            doit(node->left);
-            doit(node->right);
+            wstr += " " + to_string(node->val);
+            write(node->left);
+            write(node->right);
         }
     }
 
+    TreeNode* read(){
+        if (rvec[i] == "#") {i++; return NULL;}
+        else {
+            // cout << "rvec[" << i<<"]: " << rvec[i]<<endl;
+            TreeNode* node = new TreeNode(stoi(rvec[i++]));
+            node->left = read();
+            node->right = read();
+            return node;
+        }
+    }
+    
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        vals = "";
+        wstr = "";
         write(root);
-        cout << vals << endl;
-        return vals;
+        // cout << wstr << endl;
+        return wstr;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        return NULL;
+        rvec.clear(); i=0;
+        istringstream iss(data);
+        copy(istream_iterator<string> (iss),
+            istream_iterator<string>(),
+            back_inserter(rvec));
+        return read();
     }
 };
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
