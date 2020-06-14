56cf68854921976e26bb2d3fd6311ae59105bc60
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-06-14 14:40:16 -0400

    optimized dfs solution

--- Concatenated Words/Leetcode_472.py
+++ Concatenated Words/Leetcode_472.py
@@ -1,3 +1,24 @@
+from functools import lru_cache
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        d = set(words)
+       
+        @lru_cache() 
+        def dfs(word):
+            for i in range(1, len(word)):
+                prefix, suffix = word[:i], word[i:]
+                if prefix in d and suffix in d:
+                    return True
+                if prefix in d and dfs(suffix):
+                    return True
+                if suffix in d and dfs(prefix):
+                    return True
+            return False
+        
+        return [word for word in words if dfs(word)]
+    
+######
+
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         words.sort(key=lambda w: len(w))
