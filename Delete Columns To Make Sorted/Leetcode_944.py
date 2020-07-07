d79ab452a6505d870b5114cd4d793040e6ade80e
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-07-06 23:43:10 -0400

    a more pythonic version

--- Delete Columns To Make Sorted/Leetcode_944.py
+++ Delete Columns To Make Sorted/Leetcode_944.py
@@ -1,10 +1,14 @@
 class Solution:
     def minDeletionSize(self, A: List[str]) -> int:
-        m, n = len(A), len(A[0])
-        cnt = 0
-        for j in range(n):
-            for i in range(1,m):
-                if A[i-1][j] > A[i][j]:
-                    cnt+=1
-                    break
-        return cnt
+        try:
+            m, n = len(A), len(A[0])
+        except IndexError:
+            return 0
+        
+        return sum(
+            any(
+                A[i-1][j] > A[i][j] 
+                for i in range(1, m)
+            ) 
+            for j in range(n)
+        )
