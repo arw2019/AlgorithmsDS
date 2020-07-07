cce0c14f0958efab377f9a263f664ecac648441e
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-07-07 01:04:07 -0400

    move 2nd loop into else branch of 1st loop

--- Largest Rectangle in Histogram/Leetcode_84.py
+++ Largest Rectangle in Histogram/Leetcode_84.py
@@ -1,18 +1,13 @@
-#top 0.5%
 class Solution:
     def largestRectangleArea(self, h: List[int]) -> int:
         stack, ans = [], 0
-        i = 0
-        while i < len(h):
-            if (not stack) or (h[stack[-1]] <= h[i]):
+        N, i = len(h), 0
+        while i < N or stack:
+            if i<N and (not stack or h[stack[-1]] <= h[i]):
                 stack.append(i)
                 i+=1
             else:
                 j = stack.pop()
                 area = h[j] * (i - stack[-1] - 1 if stack else i)
                 ans = max(ans, area)
-        while stack:
-            j = stack.pop()
-            area = h[j] * (i - stack[-1] - 1 if stack else i)
-            ans = max(ans, area)
         return ans
