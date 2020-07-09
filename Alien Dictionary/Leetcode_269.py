2cdcb2209767f8b956652f80c7e9d1f5c652ba6c
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-07-08 23:50:29 -0400

    replace for loop with next(generator, default)

--- Alien Dictionary/Leetcode_269.py
+++ Alien Dictionary/Leetcode_269.py
@@ -5,11 +5,14 @@ class Solution:
         for i in range(N-1):
             word1, word2 = words[i:i+2]
             scan_length = min(len(word1), len(word2))
-            for j in range(scan_length):
-                char1, char2 = word1[j], word2[j]
-                if char1 != char2:
-                    less += [char1+char2]
-                    break
+            less += next(
+                (
+                    [word1[j]+word2[j]]
+                    for j in range(scan_length)
+                    if word1[j] != word2[j]
+                ),
+            []
+            )
                     
         chars = set(''.join(words))
         order = []
