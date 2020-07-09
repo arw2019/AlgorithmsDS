727b3d583b2078a5d3ce1a931913dc0c1f0e36ee
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-07-08 23:39:16 -0400

    simplify syntax with extra variables

--- Alien Dictionary/Leetcode_269.py
+++ Alien Dictionary/Leetcode_269.py
@@ -1,10 +1,12 @@
  
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
+        N = len(words)
         less = []
-        for i in range(len(words)-1):
+        for i in range(N-1):
             word1, word2 = words[i], words[i+1]
-            for j in range(min(len(word1), len(word2))):
+            scan_length = min(len(word1), len(word2))
+            for j in range(scan_length):
                 char1, char2 = word1[j], word2[j]
                 if char1 != char2:
                     less += [char1 + char2]
@@ -13,7 +15,8 @@ class Solution:
                     
         chars = set(''.join(words))
         order = []
-        if not less and any(len(words[i+1]) < len(words[i]) for i in range(len(words)-1)): return ''
+        if (not less and 
+            any(len(words[i+1]) < len(words[i]) for i in range(N-1))): return ''
         while less:
             free = chars - {pair[1] for pair in less}
             if not free: return ''
