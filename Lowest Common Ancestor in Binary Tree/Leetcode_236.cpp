7e8e5937ad3868b7d6ac0d24cda5d2d7f4bf7613
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-07-30 01:05:12 -0400

    O(N) solution based on postorder walk

--- Lowest Common Ancestor in Binary Tree/Leetcode_236.cpp
+++ Lowest Common Ancestor in Binary Tree/Leetcode_236.cpp
@@ -8,7 +8,43 @@
  * };
  */
 
+// O(N) time, O(h) space
+// a-la postorder traversal
+// slower on LC than recursive (presumably b/c overhead)
+
+class Solution {
+private:
+    typedef struct Status {
+        Status(): numTargets(0), ancestor(NULL) {}
+        int numTargets;
+        TreeNode* ancestor;
+    } Status;
+    
+    Status* lca_helper(TreeNode* root, TreeNode* p, TreeNode* q){
+        Status* curStatus = new Status;
+        if (root == NULL) return curStatus; 
+            
+        Status* left = lca_helper(root->left, p, q);
+        if (left->numTargets == 2) return left;
+        else curStatus->numTargets += left->numTargets;
+        
+        Status* right = lca_helper(root->right, p, q);
+        if (right->numTargets == 2) return right;
+        else curStatus->numTargets += right->numTargets;
+     
+        if ((p->val == root->val) || (q->val == root->val)) curStatus->numTargets += 1;
+        if (curStatus->numTargets == 2) curStatus->ancestor = root;
+        return curStatus;
+    };
+
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return lca_helper(root, p, q)->ancestor;
+    }
+};
+
 // O(N^2) time, O(h) space
+// recursion
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
