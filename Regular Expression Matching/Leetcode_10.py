175f37803df9effde533edaa93aef4fdb7671651
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-02 15:51:10 -0400

    simplify if statement

--- Regular Expression Matching/Leetcode_10.py
+++ Regular Expression Matching/Leetcode_10.py
@@ -11,13 +11,10 @@ class Solution:
                     dp[i][j] = True if j==0 else (p[j-1] == '*' and dp[i][j-2])
                 elif j == 0:
                     dp[i][j] = False
-                elif p[j-1] == s[i-1] or p[j-1] == '.': 
+                elif p[j-1] in (s[i-1] + '.'): 
                     dp[i][j] = dp[i-1][j-1]
                 elif p[j-1] == '*':
-                    if p[j-2] in (s[i-1], '.'):
-                        dp[i][j] = dp[i][j-2] or dp[i-1][j] or dp[i][j-1]
-                    if not dp[i][j]:
-                        dp[i][j] = dp[i][j-2]
+                        dp[i][j] = dp[i][j-2] or (p[j-2] in s[i-1] + '.') and (dp[i-1][j] or dp[i][j-1])
 
         return dp[-1][-1]
 
