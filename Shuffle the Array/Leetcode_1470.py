24c5cf350b8a97715bdc6a5a206f15e9b61d69f9
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-03 02:46:03 -0400

    fix a typo

--- Shuffle the Array/Leetcode_1470.py
+++ Shuffle the Array/Leetcode_1470.py
@@ -1,4 +1,4 @@
-import itertools
+from itertools import chain
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        return list(itertools.chain  ([(nums[i], nums[n+i]) for i in range(n)]))
+        return list(chain(*list(chain([(nums[i], nums[n+i]) for i in range(n)]))))
