8263e62b1601add4c6fb58e303b9be9a2d863a08
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-08 23:44:53 -0400

    six solutions

--- Shuffle the Array/Leetcode_1470.py
+++ Shuffle the Array/Leetcode_1470.py
@@ -1,4 +1,42 @@
-from itertools import chain
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        return list(chain(*list(chain([(nums[i], nums[n+i]) for i in range(n)]))))
+        return [nums[n+i//2] if i%2 else nums[i//2] for i in range(2*n)]
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = [None]*(2*n)
+        for i in range(2*n):
+            if i%2: 
+                res[i] = nums[n+i//2]
+            else:
+                res[i] = nums[i//2]
+        return res
+
+import itertools
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        pairs = list(zip(nums[:n], nums[n:]))
+        return list(itertools.chain(*pairs))
+
+import itertools
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        x, y = nums[:n], nums[n:] 
+        pairs = list(zip(x, y))
+        return list(itertools.chain(*pairs))
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        x, y = nums[:n], nums[n:] 
+        res = []
+        for pair in zip(x, y):
+            res += [*pair]
+        return res
+        
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        x, y = nums[:n], nums[n:] 
+        res = []
+        for i in range(n):
+            res += [x[i], y[i]]
+        return res
