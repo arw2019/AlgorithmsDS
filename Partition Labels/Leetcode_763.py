11c2840b4127c2c9974a8e157381e4b3e94f2c77
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-09 16:59:41 -0400

    add O(1) space solution

--- Partition Labels/Leetcode_763.py
+++ Partition Labels/Leetcode_763.py
@@ -1,3 +1,16 @@
+# O(N) time, O(1) space
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        last_occurence = {char: i for i, char in enumerate(S)}
+        ans, cur_start, cur_break = [], 0, last_occurence[S[0]]
+        for idx, char in enumerate(S):
+            cur_break = max(cur_break, last_occurence[char])
+            if cur_break == idx:
+                ans += [cur_break-cur_start+1]
+                cur_start = idx+1
+        return ans
+    
+
 # O(N) time, N=len(S)
 # O(L) space, L = length of alphabet
 class Solution:
