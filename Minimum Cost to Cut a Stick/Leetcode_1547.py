50d4e8e625af4963c0c6c832a64a03683e9d71cf
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-12 23:46:25 -0400

    top-down DP (LC AC)

--- Minimum Cost to Cut a Stick/Leetcode_1547.py
+++ Minimum Cost to Cut a Stick/Leetcode_1547.py
@@ -1,3 +1,34 @@
+# top-down DP
+# recursive solution with caching
+# still O(N!) complexity but much faster b/c caching
+
+from functools import lru_cache
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts.sort() 
+        # print(f"n={n}, cuts={cuts}")
+        # dp(i, j) = min cost of cut between cuts[i] & cuts[j] non-inclusive
+        # base case: dp(i, i+1) = 0
+        # recursion: dp(i, j) = min_cost (all possible cuts i:j)
+        
+        @lru_cache(maxsize=None)
+        def helper(n, cuts): 
+            if len(cuts) == 0:
+                minCost = 0
+            elif len(cuts) == 1:
+                minCost=n
+            else:
+                minCost = float('inf')
+                for i in range(len(cuts)):
+                    minCost = min(
+                        minCost, 
+                        n + helper(cuts[i], tuple(cuts[:i])) + \
+                        helper(n-cuts[i], tuple(c - cuts[i] for c in cuts[i+1:])))
+                
+            return minCost
+        
+        return helper(n, tuple(cuts))
+
 # recursive solution
 # O(N!) time complexity, N=len(cuts)
 class Solution:
