cb1995d41c5f0a3bf5e74feaa1d47f627359e07e
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-23 14:33:33 -0400

    O(N) time, O(1) space

---
+++ Jump Game/Leeetcode_45.py
@@ -0,0 +1,9 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps, curEnd, curFarthest = 0, 0, 0
+        for i in range(len(nums)-1):
+            curFarthest = max(curFarthest, i+nums[i])
+            if i == curEnd:
+                jumps += 1;
+                curEnd = curFarthest
+        return jumps
