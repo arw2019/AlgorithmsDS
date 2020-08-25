1cd6f59f2657ce4e2ee9606a75677494d82d7530
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-24 23:55:03 -0400

    recursive solution

--- Print Immutable Linked List in Reverse/Leetcode_1265.py
+++ Print Immutable Linked List in Reverse/Leetcode_1265.py
@@ -19,3 +19,23 @@ class Solution:
                         
         for node in linkedList[::-1]:
             node.printValue()
+
+ 
+# """
+# This is the ImmutableListNode's API interface.
+# You should not implement it, or speculate about its implementation.
+# """
+# class ImmutableListNode:
+#     def printValue(self) -> None: # print the value of this node.
+#     def getNext(self) -> 'ImmutableListNode': # return the next node.
+
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+        
+        def func(node: 'ImmutableListNode') -> List[int]:
+            if node is None: 
+                return []
+            else:
+                return func(node.getNext()) + [node.printValue()]
+      
+        return func(head)
