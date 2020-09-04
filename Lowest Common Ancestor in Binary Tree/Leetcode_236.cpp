727fc0f33d2691cb9032c6d34763aa4e3c6b310a
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-09-03 23:55:17 -0400

    format according to google style guide

--- Lowest Common Ancestor in Binary Tree/Leetcode_236.cpp
+++ Lowest Common Ancestor in Binary Tree/Leetcode_236.cpp
@@ -13,48 +13,54 @@
 // slower on LC than recursive (presumably b/c overhead)
 
 class Solution {
-private:
-    typedef struct Status {
-        Status(): numTargets(0), ancestor(NULL) {}
-        int numTargets;
-        TreeNode* ancestor;
-    } Status;
-    
-    Status* lca_helper(TreeNode* root, TreeNode* p, TreeNode* q){
-        Status* curStatus = new Status;
-        if (root == NULL) return curStatus; 
-            
-        Status* left = lca_helper(root->left, p, q);
-        if (left->numTargets == 2) return left;
-        else curStatus->numTargets += left->numTargets;
-        
-        Status* right = lca_helper(root->right, p, q);
-        if (right->numTargets == 2) return right;
-        else curStatus->numTargets += right->numTargets;
-     
-        if ((p->val == root->val) || (q->val == root->val)) curStatus->numTargets += 1;
-        if (curStatus->numTargets == 2) curStatus->ancestor = root;
-        return curStatus;
-    };
+ private:
+  typedef struct Status {
+    Status() : numTargets(0), ancestor(NULL) {}
+    int numTargets;
+    TreeNode* ancestor;
+  } Status;
 
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return lca_helper(root, p, q)->ancestor;
-    }
+  Status* lca_helper(TreeNode* root, TreeNode* p, TreeNode* q) {
+    Status* curStatus = new Status;
+    if (root == NULL) return curStatus;
+
+    Status* left = lca_helper(root->left, p, q);
+    if (left->numTargets == 2)
+      return left;
+    else
+      curStatus->numTargets += left->numTargets;
+
+    Status* right = lca_helper(root->right, p, q);
+    if (right->numTargets == 2)
+      return right;
+    else
+      curStatus->numTargets += right->numTargets;
+
+    if ((p->val == root->val) || (q->val == root->val))
+      curStatus->numTargets += 1;
+    if (curStatus->numTargets == 2) curStatus->ancestor = root;
+    return curStatus;
+  };
+
+ public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    return lca_helper(root, p, q)->ancestor;
+  }
 };
 
 // O(N^2) time, O(h) space
 // recursion
 class Solution {
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if ((root == NULL) || (root==p) || (root==q)) return root;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if ((left != NULL) && (right != NULL)){
-            return root;
-        } else if (left != NULL) return left;
-        else return right;
-    }
+ public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if ((root == NULL) || (root == p) || (root == q)) return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    if ((left != NULL) && (right != NULL)) {
+      return root;
+    } else if (left != NULL)
+      return left;
+    else
+      return right;
+  }
 };
-
