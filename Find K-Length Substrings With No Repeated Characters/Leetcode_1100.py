ac82dbfe8446822b000ad1f25396f0067f28e981
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-09-13 14:56:08 -0400

    Two more solutions (Counter, pure set)

--- Find K-Length Substrings With No Repeated Characters/Leetcode_1100.py
+++ Find K-Length Substrings With No Repeated Characters/Leetcode_1100.py
@@ -1,3 +1,32 @@
+class Solution:
+    def numKLenSubstrNoRepeats(self, S: str, K: int) -> int:
+        ans, i = 0, 0
+        chars = set()
+        for j in range(len(S)):
+            while S[j] in chars:
+                chars.remove(S[i])
+                i += 1
+            chars.add(S[j])
+            ans += j - i + 1 >= K
+        return ans
+
+class Solution:
+    def numKLenSubstrNoRepeats(self, S: str, K: int) -> int:
+        N = len(S)
+        if len(S) < K: return 0
+        
+        ans = 0
+        cnt = collections.Counter(S[:K])
+        for i in range(N-K+1):
+            _, freq = cnt.most_common()[0] 
+            if freq == 1:
+                ans += 1
+            if i+K<N:
+                cnt[S[i]] -= 1
+                cnt[S[i+K]] += 1
+       
+        return ans
+
 from collections import deque, defaultdict
 class Solution:
     def numKLenSubstrNoRepeats(self, S: str, K: int) -> int:
