f335026f675aeae5d5d126c645b7cb9ba635808e
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-09-14 23:59:59 -0400

    Update Leetcode_445.py

--- Add Two Numbers II/Leetcode_445.py
+++ Add Two Numbers II/Leetcode_445.py
@@ -5,11 +5,10 @@
 #         self.next = None
 
 # O(N1 + N2) time where N1, N2 are lengths of inputs
-# O(1) extra space 
+# O(1) extra space
 
 # top 0.7%
 class Solution:
-    
     def reverseList(self, head: ListNode) -> ListNode:
         cur, prev = head, None
         while cur:
@@ -17,17 +16,17 @@ class Solution:
             cur.next = prev
             prev, cur = cur, nextNode
         return prev
-    
+
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
+
         l1, l2 = self.reverseList(l1), self.reverseList(l2)
-        
+
         dummy = ListNode(-1)
         res = dummy
         carryover = 0
         while l1 or l2:
             cur = carryover
-            if l1: 
+            if l1:
                 cur += l1.val
                 l1 = l1.next
             if l2:
@@ -36,6 +35,7 @@ class Solution:
             cur, carryover = cur % 10, cur // 10
             res.next = ListNode(cur)
             res = res.next
-        if carryover: res.next = ListNode(carryover)
-        
+        if carryover:
+            res.next = ListNode(carryover)
+
         return self.reverseList(dummy.next)
